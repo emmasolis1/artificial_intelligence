c84123dfb5968610450bf9067a40fb387e3c9513
Emmanuel S <emmanuel.solisp@icloud.com>
2022-05-17 13:46:55 -0600

    new: added request_topology func

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -8,3 +8,23 @@ void UserInterface::print_welcome() {
         )" << '\n';
 }
 
+vector<int> UserInterface::request_topology_info() {
+    vector<int> toplogy;
+    int answer = 0;
+    cout<<"How many input neurons?"<<endl;
+    cin>>answer;
+    toplogy.push_back(answer);
+    cout<<"How many hidden layers?"<<endl;
+    int hidden_layers = 0;
+    cin>>hidden_layers;
+    for (short i=0; i<hidden_layers; i++) {
+        cout<<"How many neurons in the hidden layer "<<i+1<<"?"<<endl;
+        cin>>answer;
+        toplogy.push_back(answer);
+    }
+    cout<<"How many output neurons?"<<endl;
+    cin>>answer;
+    toplogy.push_back(answer);
+
+    return toplogy;
+}
